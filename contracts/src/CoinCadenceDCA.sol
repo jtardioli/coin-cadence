d477198fa39fa5297f828140cffdcd014d3c6104
jtardioli <joshua.tardioli@gmail.com>
2024-05-03 22:01:56 -0300

    weird remapping to deal with broken  openzeppelin imports in v3-periphery

--- contracts/src/CoinCadenceDCA.sol
+++ contracts/src/CoinCadenceDCA.sol
@@ -1,31 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.6;
+pragma solidity =0.7.6;
+pragma abicoder v2;
 
 import {ISwapRouter} from "../lib/v3-periphery-foundry/contracts/interfaces/ISwapRouter.sol";
+import {TransferHelper} from "../lib/v3-periphery-foundry/contracts/libraries/TransferHelper.sol";
 
 contract CoinCadenceDCA {
-    ISwapRouter router;
+    ISwapRouter public immutable swapRouter;
 
-    /**
-     *
-     * How will we charge users for the gas of the swap?
-     * We need to be able to set a limit
-     *
-     * @param from address to take the input token from
-     * @param to address to send the output token to
-     * @param inputToken address of the token to be used as input
-     * @param outputToken address of the token to be used as output
-     * @param amountInputToken amount of input token to be used
-     * @param slippage slippage in percentage
-     * @param gasLimit gas limit for the transaction
-     */
-    function investFromWallet(
-        address from,
-        address to,
-        address inputToken,
-        address outputToken,
-        uint256 amountInputToken,
-        uint256 slippage,
-        uint256 gasLimit
-    ) external payable {}
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    // For this example, we will set the pool fee to 0.3%.
+    uint24 public constant poolFee = 3000;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
+    /// @notice swapExactInputSingle swaps a fixed amount of DAI for a maximum possible amount of WETH9
+    /// using the DAI/WETH9 0.3% pool by calling `exactInputSingle` in the swap router.
+    /// @dev The calling address must approve this contract to spend at least `amountIn` worth of its DAI for this function to succeed.
+    /// @param amountIn The exact amount of DAI that will be swapped for WETH9.
+    /// @return amountOut The amount of WETH9 received.
+    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
+        // msg.sender must approve this contract
+
+        // Transfer the specified amount of DAI to this contract.
+        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
+
+        // Approve the router to spend DAI.
+        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
+    }
 }
