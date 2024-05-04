cb610814e9a8f9b9fc762f1729b6b9dbe98960a9
jtardioli <joshua.tardioli@gmail.com>
2024-05-03 22:38:42 -0300

    save

--- contracts/src/CoinCadenceDCA.sol
+++ contracts/src/CoinCadenceDCA.sol
@@ -5,12 +5,9 @@ pragma abicoder v2;
 import {ISwapRouter} from "../lib/v3-periphery-foundry/contracts/interfaces/ISwapRouter.sol";
 import {TransferHelper} from "../lib/v3-periphery-foundry/contracts/libraries/TransferHelper.sol";
 
+// DONT FOR GET TO LOOK INTO PRICE ORACLE TO NOT GET FRONT RUN
 contract CoinCadenceDCA {
     ISwapRouter public immutable swapRouter;
-
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
@@ -19,18 +16,34 @@ contract CoinCadenceDCA {
         swapRouter = _swapRouter;
     }
 
-    /// @notice swapExactInputSingle swaps a fixed amount of DAI for a maximum possible amount of WETH9
-    /// using the DAI/WETH9 0.3% pool by calling `exactInputSingle` in the swap router.
-    /// @dev The calling address must approve this contract to spend at least `amountIn` worth of its DAI for this function to succeed.
-    /// @param amountIn The exact amount of DAI that will be swapped for WETH9.
-    /// @return amountOut The amount of WETH9 received.
-    function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
-        // msg.sender must approve this contract
+    /// @notice swapExactInputSingle swaps a fixed amount of inputToken for a maximum possible amount of outputToken
+    /// using the inputToken/outputToken 0.3% pool by calling `exactInputSingle` in the swap router.
+    /// @dev The calling address must approve this contract to spend at least `amountIn` worth of its inputToken for this function to succeed.
+    /// @param amountIn The exact amount of inputToken that will be swapped for outputToken.
+    /// @return amountOut The amount of outputToken received.
+    function swapExactInputSingle(address inputToken, address outputToken, address recipient, uint256 amountIn)
+        external
+        returns (uint256 amountOut)
+    {
+        // Transfer the specified amount of inputToken to this contract.
+        TransferHelper.safeTransferFrom(inputToken, msg.sender, address(this), amountIn);
 
-        // Transfer the specified amount of DAI to this contract.
-        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
+        // Approve the router to spend inputToken.
+        TransferHelper.safeApprove(inputToken, address(swapRouter), amountIn);
+        // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
+        // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: inputToken,
+            tokenOut: outputToken,
+            fee: poolFee,
+            recipient: recipient,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
 
-        // Approve the router to spend DAI.
-        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
+        // The call to `exactInputSingle` executes the swap.
+        amountOut = swapRouter.exactInputSingle(params);
     }
 }
