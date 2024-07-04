f26e192148c78aa3db3f87b5986a89e7636f9db5
jtardioli <joshua.tardioli@gmail.com>
2024-07-04 08:00:40 -0400

    WIP: debugging router

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,13 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { Percent, Token } from "@uniswap/sdk-core";
+import {
+  Percent,
+  Token,
+  TradeType,
+  CurrencyAmount,
+  ChainId,
+} from "@uniswap/sdk-core";
 import { ethers } from "ethers";
 import {
   AlphaRouter,
@@ -21,24 +27,85 @@ app.use(helmet());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// interface ExampleConfig {
-//   env: Environment
-//   rpc: {
-//     local: string
-//     mainnet: string
-//   }
-//   wallet: {
-//     address: string
-//     privateKey: string
-//   }
-//   tokens: {
-//     in: Token
-//     amountIn: number
-//     out: Token
-//   }
-// }
+export const V3_SWAP_ROUTER_ADDRESS =
+  "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
+export const WETH_CONTRACT_ADDRESS =
+  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 
-// export const CurrentConfig: ExampleConfig = {...}
+// Currencies and Tokens
+
+export const USDC_TOKEN = new Token(
+  ChainId.MAINNET,
+  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  6,
+  "USDC",
+  "USD//C"
+);
+
+export const DAI_TOKEN = new Token(
+  ChainId.MAINNET,
+  "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+  18,
+  "DAI",
+  "Dai Stablecoin"
+);
+
+export const WETH_TOKEN = new Token(
+  ChainId.MAINNET,
+  "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  18,
+  "WETH",
+  "Wrapped Ether"
+);
+
+export enum Environment {
+  LOCAL,
+  WALLET_EXTENSION,
+  MAINNET,
+}
+
+// Inputs that configure this example to run
+export interface ExampleConfig {
+  env: Environment;
+  rpc: {
+    local: string;
+    mainnet: string;
+  };
+  wallet: {
+    address: string;
+    privateKey: string;
+  };
+  tokens: {
+    in: Token;
+    amountIn: number;
+    out: Token;
+  };
+}
+
+export const CurrentConfig: ExampleConfig = {
+  env: Environment.LOCAL,
+  rpc: {
+    local: "http://localhost:8545",
+    mainnet: "",
+  },
+  wallet: {
+    address: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266",
+    privateKey:
+      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  },
+  tokens: {
+    in: WETH_TOKEN,
+    amountIn: 1,
+    out: USDC_TOKEN,
+  },
+};
+
+function countDecimals(x: number) {
+  if (Math.floor(x) === x) {
+    return 0;
+  }
+  return x.toString().split(".")[1].length || 0;
+}
 
 export function fromReadableAmount(amount: number, decimals: number): JSBI {
   const extraDigits = Math.pow(10, countDecimals(amount));
@@ -52,19 +119,20 @@ export function fromReadableAmount(amount: number, decimals: number): JSBI {
   );
 }
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.send("Application is running");
 });
 
-app.get("/api/v1", (req, res) => {
+app.get("/api/v1", async (req, res) => {
   const rpcUrl = process.env.MAINNET_RPC_URL;
-  const mainnetChainId = 1;
+
+  console.log({ rpcUrl });
 
   const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
 
   const router = new AlphaRouter({
-    chainId: mainnetChainId,
-    provider,
+    chainId: ChainId.MAINNET,
+    provider: provider,
   });
 
   const options: SwapOptionsSwapRouter02 = {
@@ -74,17 +142,52 @@ app.get("/api/v1", (req, res) => {
     type: SwapType.SWAP_ROUTER_02,
   };
 
-  const rawTokenAmountIn: JSBI = fromReadableAmount(
-    CurrentConfig.currencies.amountIn,
-    CurrentConfig.currencies.in.decimals
+  const readableAmount = fromReadableAmount(
+    CurrentConfig.tokens.amountIn,
+    CurrentConfig.tokens.in.decimals
+  ).toString();
+
+  const amount = CurrencyAmount.fromRawAmount(
+    CurrentConfig.tokens.in,
+    readableAmount
   );
 
-  const route = await router.route(
-    CurrencyAmount.fromRawAmount(CurrentConfig.currencies.in, rawTokenAmountIn),
-    CurrentConfig.currencies.out,
-    TradeType.EXACT_INPUT,
-    options
-  );
+  try {
+    const route = await router.route(
+      amount,
+      CurrentConfig.tokens.out,
+      TradeType.EXACT_INPUT,
+      options
+    );
+
+    if (!route || !route.methodParameters) {
+      console.log("No route found");
+    }
+
+    console.log({ route });
+  } catch (error) {
+    console.log({ error });
+  }
+
+  // const wallet = new ethers.Wallet(privateKey, provider);
+  // const tokenContract = new ethers.Contract(
+  //   CurrentConfig.tokens.in.address,
+  //   ERC20ABI,
+  //   wallet
+  // );
+  // const tokenApproval = await tokenContract.approve(
+  //   V3_SWAP_ROUTER_ADDRESS,
+  //   ethers.BigNumber.from(rawTokenAmountIn.toString())
+  // );
+
+  // const txRes = await wallet.sendTransaction({
+  //   data: route.methodParameters.calldata,
+  //   to: V3_SWAP_ROUTER_ADDRESS,
+  //   value: route.methodParameters.value,
+  //   from: wallet.address,
+  //   maxFeePerGas: MAX_FEE_PER_GAS,
+  //   maxPriorityFeePerGas: MAX_PRIORITY_FEE_PER_GAS,
+  // })
 
   res.send("Application is running");
 });
