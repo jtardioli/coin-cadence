3a2f2c210637bed9e8f61254d026f4a7b0307007
jtardioli <joshua.tardioli@gmail.com>
2024-07-12 07:54:18 -0400

    save

--- contracts/test/CoinCadenceDCA.test.sol
+++ contracts/test/CoinCadenceDCA.test.sol
@@ -10,6 +10,7 @@ contract CoinCadenceDCATest is Test {
     address public user = makeAddr("user");
 
     address public swapRouterAddr = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address public wethAddress = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
 
     CoinCadenceDCA coinCadenceDCA;
 
@@ -17,10 +18,13 @@ contract CoinCadenceDCATest is Test {
         coinCadenceDCA = new CoinCadenceDCA(swapRouterAddr);
     }
 
+    // deal user the token
+    // approve my contract to spend the token
+    // transfer the token to my contract
+    // approve swap router to spend the token
+    // call exactInput
     function test() public {
-        address alice = makeAddr("alice");
-        emit log_address(alice);
-        vm.deal(alice, 1 ether);
+        vm.deal(user, 1 ether);
         address amount = coinCadenceDCA.exactInput(
             ISwapRouter.ExactInputParams({
                 path: hex"2260FAC5E5542a773Aa44fBCfeDf7C193bc2C59900000aC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc200000aA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
